7705b45b5abcfc764bf7e1010ced1deb103c91f7
AnwarSahid <anwarsahid28oktober@gmail.com>
2022-12-24 11:53:40 +0700

    model

---
+++ src/models/user.model.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define('users', {
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+
+    })
+
+    return User
+}
